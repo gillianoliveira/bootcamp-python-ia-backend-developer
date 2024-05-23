57fd92ac5bd19bcfdeb127bddfff236b1f072b95
Gillian Gomes <brgillian@gmail.com>
2024-05-23 18:04:14 -0300

    programa finalizado

--- desafio-projeto-01-sistema-bancario/d1-criando-um-sistema-bancario.py
+++ desafio-projeto-01-sistema-bancario/d1-criando-um-sistema-bancario.py
@@ -17,6 +17,8 @@ saldo = 0.0
 valor = 0.0
 extrato = []
 depositos = []
+NUM_SAQUES = 3
+LIMITE_SAQUES = 500.00
 
 while True:
     opcao = int(input('''
@@ -35,14 +37,40 @@ Opção escolhida: '''))
         else:
             print('Valor inválido. Depósito não realizado.')        
     elif opcao == 2:
-        print("Sacar")
-    elif opcao == 3:
-        print("Extrato")
+        conta_saques = len(extrato)
+        valor = float(input('Informe o valor do saque: R$ '))
         
+        if conta_saques > NUM_SAQUES:
+            print(f'Você excedeu o limite de {NUM_SAQUES} saques diários. Operação não realizada.')
+        elif valor > LIMITE_SAQUES:
+            print(f'Esse valor excede o limite de R$ {LIMITE_SAQUES} por saque. Operação não realizada.')
+        elif valor > saldo:
+            print(f'Você não tem saldo suficiente para efetuar esta operação. Operação não realizada.')
+        else:
+            saldo -= valor
+            data = dt.datetime.now().strftime("%Y-%m-%d %I:%M:%S")
+            extrato.append({'data': data, 'valor': valor, 'operacao': 'Saque'})            
+    elif opcao == 3:
+        total_depositos = 0.0
+        total_saques = 0.0
+        print("Extrato")                
         for operacao in extrato:
-            print(f"{operacao['data']} - {operacao['operacao']}: R$ {operacao['valor']:.2f}")
-                
+            print(f"{operacao['data']} => R$ {operacao['valor']:.2f} - {operacao['operacao']}")
+            
+            if operacao['operacao'] == 'Depósito':
+                total_depositos += operacao['valor']
+            elif operacao['operacao'] == 'Saque':
+                total_saques += operacao['valor']
+        
+        print('-' * 30)
+        print(f'Total de depósitos: R$ {total_depositos:.2f}')
+        print(f'Total de saques: R$ {total_saques:.2f}')
+        print(f'Saldo: R$ {saldo:.2f}')
+            
     elif opcao == 4:
-        print("Progama encerrado.")        
+        print("Progama encerrado.")
+        exit()
+                
     else:
         print("Opção inválida. Programa encerrado")
+        exit()
