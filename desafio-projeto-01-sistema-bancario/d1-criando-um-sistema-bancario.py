452d45cfa323eb1b76c545aca98387e4cf1ca6cb
Gillian Gomes <brgillian@gmail.com>
2024-05-23 15:47:39 -0300

    funcionalidade deposito adicionada

--- desafio-projeto-01-sistema-bancario/d1-criando-um-sistema-bancario.py
+++ desafio-projeto-01-sistema-bancario/d1-criando-um-sistema-bancario.py
@@ -6,25 +6,42 @@ Bootcamp: Python AI Backend Developer Bootcamp
 Conclusão do Desafio: 23-05-2024
 Proposta no README
 '''
+import datetime as dt
 
-print('-----------------------------------------------------')
-print('                         MENU                        ')
-print('-----------------------------------------------------')
+print('----------------------------------------')
+print('                 MENU                   ')
+print('----------------------------------------')
 
-opcao = int(input('''
+# Variáveis Globais
+saldo = 0.0
+valor = 0.0
+extrato = []
+depositos = []
+
+while True:
+    opcao = int(input('''
 [1] Depositar
 [2] Sacar
 [3] Exibir Extrato
 [4] Sair
 Opção escolhida: ''')) 
-
-while True:
+    
     if opcao == 1:
-        print("Depositar")
+        valor = float(input('Informe o valor do depósito: R$ '))
+        if valor > 0:
+            saldo += valor
+            data = dt.datetime.now().strftime("%Y-%m-%d %I:%M:%S")
+            extrato.append({'data': data, 'valor': valor, 'operacao': 'Depósito'})
+        else:
+            print('Valor inválido. Depósito não realizado.')        
     elif opcao == 2:
         print("Sacar")
     elif opcao == 3:
-        print("Exibir Extrato")        
+        print("Extrato")
+        
+        for operacao in extrato:
+            print(f"{operacao['data']} - {operacao['operacao']}: R$ {operacao['valor']:.2f}")
+                
     elif opcao == 4:
         print("Progama encerrado.")        
     else:
