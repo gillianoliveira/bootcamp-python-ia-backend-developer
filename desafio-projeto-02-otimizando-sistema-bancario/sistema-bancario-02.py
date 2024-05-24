0aba9f1f2f2c510bf56dda2cc3b8bf086eedfdf9
Gillian Gomes <brgillian@gmail.com>
2024-05-24 14:17:20 -0300

    validacao cpf no cadastro de clientes

--- desafio-projeto-02-otimizando-sistema-bancario/sistema-bancario-02.py
+++ desafio-projeto-02-otimizando-sistema-bancario/sistema-bancario-02.py
@@ -6,14 +6,55 @@ Bootcamp: Python AI Backend Developer Bootcamp
 Conclusão do Desafio: ?
 Proposta no README
 '''
-
+# Função consmética para gerar o título de cada seção
 def titulo(t):
     traco = '-'
     print(traco * 30)
     print(f"{t.center(30, ' ')}")
     print(traco * 30)
 
+def depositar():
+    pass
 
+def sacar():
+    pass
+
+def visualizar_extrato():
+    pass
+
+def cadastrar_cliente():
+    global clientes
+    while True:
+        # Solicitar o cpf
+        cpf = int(input("CPF: "))
+        
+        # Verificar a quantidade de dígitos do cpf
+        if len(str(cpf)) != 11:
+            print('O CPF deve ter 11 dígitos. Digite novamente. ')
+            continue
+        
+        # Verificar se o cpf já está cadastrado
+        for cliente in clientes:
+            if cliente["cpf"] == cpf:
+                print('Cliente já cadastrado.')
+                menu()
+                return
+            else:
+                nome = input('Nome completo: ')
+                data_nascimento = input('Data de nascimento: ')
+                logradouro = input('Digite o logradouro: ')
+                numero = input('Digite o número: ')
+                bairro = input('Bairro: ')
+                cidade = input('Cidade: ')
+                estado = input('Sigla do estado: ')
+        
+        # Armazenar os dados do cliente
+        clientes.append({"cpf": cpf, "nome": nome, "data_nascimento": data_nascimento, "logradouro": logradouro, "numero":numero, "bairro": bairro, "cidade":cidade, "estado":estado})
+        
+def criar_conta():
+    pass
+
+# Função principal que chama todas as outras
 def menu():
     opcao_menu = int(input('''
     [1] Depositar
@@ -26,24 +67,24 @@ def menu():
     
     match opcao_menu:
         case 1:
-            print("Depositar")
+            depositar()
         case 2:
-            print("Sacar")
+            sacar()
         case 3:
-            print("Visualisar extrato")
+            visualizar_extrato()
         case 4:
-            print("Cadastrar Usuário")
+            cadastrar_cliente()
         case 5:
-            print("Cadastrar Nova Conta")
+            criar_conta()
         case 6:
             print("Sair")
         case _:
             print('Opção inválida')
-    
-    
 
-
+# Variáveis Globais
+clientes = []
 
 # Entrada do Programa
 titulo('Menu')
 menu()
+
