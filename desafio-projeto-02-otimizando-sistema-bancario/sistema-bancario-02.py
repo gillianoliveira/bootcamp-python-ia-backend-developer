9b45e004455bb130efa4ca98f204bc1b46b8736b
Gillian Gomes <brgillian@gmail.com>
2024-05-24 20:08:40 -0300

    Finalizacao da funcao de cadastro de conta

--- desafio-projeto-02-otimizando-sistema-bancario/sistema-bancario-02.py
+++ desafio-projeto-02-otimizando-sistema-bancario/sistema-bancario-02.py
@@ -6,6 +6,9 @@ Bootcamp: Python AI Backend Developer Bootcamp
 Conclusão do Desafio: ?
 Proposta no README
 '''
+
+import datetime
+
 # Função consmética para gerar o título de cada seção
 def titulo(t):
     traco = '-'
@@ -23,6 +26,7 @@ def visualizar_extrato():
     pass
 
 def cadastrar_cliente():
+    titulo('Cadastro de Clientes')
     global clientes
     while True:
         # Solicitar o cpf
@@ -30,8 +34,9 @@ def cadastrar_cliente():
         
         # Verificar a quantidade de dígitos do cpf
         if len(str(cpf)) != 11:
-            print('O CPF deve ter 11 dígitos. Digite novamente. ')
-            continue
+            print('O CPF deve ter 11 dígitos. ')
+            menu()
+            return
         
         # Verificar se o cpf já está cadastrado
         for cliente in clientes:
@@ -39,30 +44,64 @@ def cadastrar_cliente():
                 print('Cliente já cadastrado.')
                 menu()
                 return
-            else:
-                nome = input('Nome completo: ')
-                data_nascimento = input('Data de nascimento: ')
-                logradouro = input('Digite o logradouro: ')
-                numero = input('Digite o número: ')
-                bairro = input('Bairro: ')
-                cidade = input('Cidade: ')
-                estado = input('Sigla do estado: ')
         
+        nome = input('Nome completo: ')
+        data_nascimento = input('Data de nascimento(dd/mm/yyyy): ')
+        logradouro = input('Logradouro: ')
+        numero = input('Número: ')
+        bairro = input('Bairro: ')
+        cidade = input('Cidade: ') 
+        estado = input('Sigla do estado: ').capitalize()     
+             
         # Armazenar os dados do cliente
         clientes.append({"cpf": cpf, "nome": nome, "data_nascimento": data_nascimento, "logradouro": logradouro, "numero":numero, "bairro": bairro, "cidade":cidade, "estado":estado})
-        
+        print('Cliente cadastrado com sucesso.')
+        menu()
+        return
+    
 def criar_conta():
+    global clientes
+    global contas   
+    numero_conta = 0
+    AGENCIA = '0001'
+    TIPO_CONTA = 'conta-corrente'
+    
+    cpf_titular = int(input('CPF do Titular: '))  # Informe o cpf do titular 
+    
+    titular_cadastrado = False
+    for cliente in clientes:
+        if cliente['cpf'] == cpf_titular:
+            titular_cadastrado = True
+            numero_conta += 1
+            conta = {'cpf_titular':cpf_titular, 'tipo_conta': TIPO_CONTA,'agencia':AGENCIA,'numero_conta':numero_conta}
+            contas.append(conta)
+            menu()
+            break
+    if not titular_cadastrado:
+        print('Você deve fazer o cadastro do titular antes de abrir a conta.')
+        menu()
+        return
+   
+  
+def listar_contas():
     pass
+
+def listar_clientes():
+    pass
+
+def sair():
+    print('Programa encerrado.')
+    exit()
 
 # Função principal que chama todas as outras
 def menu():
+    titulo('Menu')
     opcao_menu = int(input('''
     [1] Depositar
     [2] Sacar
     [3] Visualizar Extrato
-    [4] Cadastrar Usuário
-    [5] Criar nova conta
-    [6] Sair
+    [4] Administrativo
+    [5] Sair
     Opção escolhida: '''))
     
     match opcao_menu:
@@ -73,18 +112,48 @@ def menu():
         case 3:
             visualizar_extrato()
         case 4:
-            cadastrar_cliente()
+            menu_admin()
         case 5:
-            criar_conta()
-        case 6:
             print("Sair")
         case _:
             print('Opção inválida')
 
+def menu_admin():
+    titulo('Administrativo')
+    opcao_menu = int(input('''
+    [1] Cadastrar Cliente
+    [2] Listar Clientes
+    [3] Criar Conta
+    [4] Listar Contas
+    [5] Menu anterior
+    [6] Sair    
+    Opção escolhida: '''))
+    
+    match opcao_menu:
+        case 1:
+            cadastrar_cliente()
+        case 2:
+            listar_clientes()
+        case 3:
+            criar_conta()
+        case 4:
+            listar_contas()
+        case 5:
+            menu()
+        case 6:
+            sair()
+        case _:
+            print('Opção inválida.')
+            sair()    
+    
+    
+
 # Variáveis Globais
 clientes = []
+numero_conta = 0
+contas = []
 
 # Entrada do Programa
-titulo('Menu')
+# Chamando a função menu que chama as demais
 menu()
 
