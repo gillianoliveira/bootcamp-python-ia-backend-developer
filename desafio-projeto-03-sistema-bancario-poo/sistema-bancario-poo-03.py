c6c592155c880140711632442effb66323ec0b39
Gillian Gomes <brgillian@gmail.com>
2024-05-28 00:14:03 -0300

    Estrutura inicial da classe conta

--- desafio-projeto-03-sistema-bancario-poo/sistema-bancario-poo-03.py
+++ desafio-projeto-03-sistema-bancario-poo/sistema-bancario-poo-03.py
@@ -7,3 +7,32 @@ Conclusão do Desafio: Em andamento
 Objetivo no README
 '''
 
+class Conta:
+    def __init__(self, numero_conta, correntista):
+        self.agencia = '001'
+        self.saldo = 0.0
+        self.numero_conta = numero_conta
+        self.correntista = correntista
+        self.historico = Historico()
+        
+    def abertura_conta():
+        pass
+    
+    def sacar():
+        pass
+    
+    def depositar():
+        pass
+    
+    def saldo():
+        pass
+    
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+
+class Historico:
+    pass
+
+
+class cliente:
+    pass
