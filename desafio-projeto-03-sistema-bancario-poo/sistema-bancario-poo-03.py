26f9dba547ae0b2082f8cf0c12b4424b88bb22ad
Gillian Gomes <brgillian@gmail.com>
2024-06-02 21:27:37 -0300

    Recomecando o codigo!

--- desafio-projeto-03-sistema-bancario-poo/sistema-bancario-poo-03.py
+++ desafio-projeto-03-sistema-bancario-poo/sistema-bancario-poo-03.py
@@ -1,41 +1,8 @@
-'''
-DESAFIO DE PROJETO 03
+'''DESAFIO DE PROJETO 03
 Modelando um Sistema Bancário em POO com Python
-Módulo: Orientação a Objetos e Boas Práticas em Python	
+Módulo: Orientação a Objetos e Boas Práticas em Python
 Bootcamp: Python AI Backend Developer
 Conclusão do Desafio: Em andamento
 Objetivo no README
 '''
 
-class Conta:
-    def __init__(self, numero_conta, correntista):
-        self.agencia = '001'
-        self.saldo = 0.0
-        self.numero_conta = numero_conta
-        self.correntista = correntista
-        self.historico = Historico()
-        
-    def abertura_conta(self, cliente = Cliente):
-        pass
-    
-    
-    def sacar(self):
-        pass
-    
-    def depositar(self):
-        pass
-    
-    def saldo(self):
-        pass
-    
-    def __str__(self):
-        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
-
-
-
-class Historico:
-    pass
-
-
-class Cliente:
-    pass
