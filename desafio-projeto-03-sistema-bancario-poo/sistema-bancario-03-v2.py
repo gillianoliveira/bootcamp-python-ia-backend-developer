99ab35d89cd5c092b210c7bb52fad63ac85ce6da
Gillian Gomes <gillianoliveiradev@gmail.com>
2024-06-06 10:09:34 -0300

    Criacao do menu principal

--- desafio-projeto-03-sistema-bancario-poo/sistema-bancario-03-v2.py
+++ desafio-projeto-03-sistema-bancario-poo/sistema-bancario-03-v2.py
@@ -8,9 +8,55 @@ def titulo(t):
     print(f"{traco * 30}")
 
 
-class Menu_Principal:
-    print('Recomeçando...')
+class MenuPrincipal:
+    def __init__(self):
+        pass
+
+    def menu(self):
+        titulo('Menu')
+        while True:
+            opcao = int(input('''
+            [1] Depositar
+            [2] Sacar
+            [3] Extrato
+            [4] Cadastrar Cliente
+            [5] Abertura de Conta
+            [6] Listar Clientes
+            [7] Listar Contas
+            [8] Sair
+            Opção escolhida: '''))
+            try:
+                match opcao:
+                    case 1:
+                        pass
+                        # depositar()
+                    case 2:
+                        pass
+                        # sacar()
+                    case 3:
+                        pass
+                        # visualizar_extrato()
+                    case 4:
+                        pass
+                    case 5:
+                        pass
+                    case 6:
+                        pass
+                    case 7:
+                        pass
+                        # listar_contas()
+                    case 8:
+                        titulo('Sair')
+                        print('Programa encerrado.')
+                        exit()
+                    case _:
+                        print('Opção inválida.')
+                        exit()
+            except ValueError as e:
+                print(f'Erro de validação: {e}')
+                continue
 
 
 # Início do programa
-menu = Menu_Principal()
+menu = MenuPrincipal()
+menu.menu()
