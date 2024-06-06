877b7951a3774904ec6979970a7cbdecc300e1a3
Gillian Gomes <gillianoliveiradev@gmail.com>
2024-06-06 09:55:58 -0300

    Recomecando

---
+++ desafio-projeto-03-sistema-bancario-poo/sistema-bancario-03-v2.py
@@ -0,0 +1,16 @@
+
+
+# Formata o título das seções
+def titulo(t):
+    traco = '-'
+    print(f"{traco * 30}")
+    print(f'{t.center(30, " ")}')
+    print(f"{traco * 30}")
+
+
+class Menu_Principal:
+    print('Recomeçando...')
+
+
+# Início do programa
+menu = Menu_Principal()
