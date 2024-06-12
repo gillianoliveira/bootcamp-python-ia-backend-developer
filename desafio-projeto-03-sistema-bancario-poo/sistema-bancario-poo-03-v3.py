17def5243f7f9f969c9759defa4f07a7817ae930
Gillian Gomes <gillianoliveiradev@gmail.com>
2024-06-12 12:23:02 -0300

    Campos do cadastro de pessoa fisica add

--- desafio-projeto-03-sistema-bancario-poo/sistema-bancario-poo-03-v3.py
+++ desafio-projeto-03-sistema-bancario-poo/sistema-bancario-poo-03-v3.py
@@ -13,37 +13,103 @@ class Estilo:
         print(f'{t.center(30, " ")}')
         print("-" * 30)
 
+
+class Cliente:
+
+    def __init__(self, endereco=None, telefone=None):
+        self.__endereco = endereco
+        self.__telefone = telefone
+
+    @property
+    def endereco(self):
+        return self.__endereco
+
+    @endereco.setter
+    def endereco(self, endereco):
+        self.__endereco = endereco
+
+    @property
+    def telefone(self):
+        return self.__telefone
+
+    @telefone.setter
+    def telefone(self, telefone):
+        self.__telefone = telefone
+
+    def cadastrar_cliente(self):
+        raise NotImplementedError("A classe filha tem que implementar este método.")
+
+
+class PessoaFisica(Cliente):
+
+    def __init__(self, nome=None, cpf=None, data_de_nascimento=None,
+                 endereco=None, telefone=None):
+        super().__init__(endereco, telefone)
+        self.__nome = nome
+        self.__cpf = cpf
+        self.__data_de_nascimento = data_de_nascimento
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @property
+    def cpf(self):
+        return self.__cpf
+
+    @cpf.setter
+    def cpf(self, cpf):
+        self.__cpf = cpf
+
+    @property
+    def data_de_nascimento(self):
+        return self.__data_de_nascimento
+
+    @data_de_nascimento.setter
+    def data_de_nascimento(self, data_de_nascimento):
+        self.__data_de_nascimento = data_de_nascimento
+
+    def cadastrar_cliente(self):
+        self.cpf = input("CPF: ")
+        self.nome = input("Nome Completo: ")
+        self.data_de_nascimento = input("Data de Nascimento: ")
+        self.telefone = input("Telefone: ")
+        self.endereco = input("Endereço: ")
+        return self
+
+
 class MenuPrincipal:
-    def __init__(self):
+    def __init__(self, cliente, pessoa_fisica):
         self._cliente = cliente
+        self._pessoa_fisica = pessoa_fisica
         # self._conta = conta
 
     def menu(self):
         while True:
             Estilo.titulo("Menu")
-            opcao = int(
-                input(
-                    """
+            try:
+                opcao = int(input("""
         [1] Abertura de Conta
         [2] Cadastrar Cliente
         [3] Listar Clientes
         [4] Listar Contas
         [8] Sair
-        Opção escolhida: """
-                )
-            )
-            try:
+        Opção escolhida: """))
                 match opcao:
                     case 1:
                         pass
                     case 2:
-                        pass
+                        self._pessoa_fisica.cadastrar_cliente()
                     case 3:
                         pass
                     case 4:
                         pass
                     case 8:
-                        pass
+                        Estilo.titulo("Sair")
                         print("Programa encerrado.")
                         exit()
                     case _:
@@ -53,7 +119,8 @@ class MenuPrincipal:
                 print(f"Opção inválida. Erro: {ve}")
 
 
-# cliente = Cliente()
+cliente = Cliente()
+pessoa_fisica = PessoaFisica()
 # conta = Conta()
-iniciar = MenuPrincipal()
+iniciar = MenuPrincipal(cliente, pessoa_fisica)
 iniciar.menu()
